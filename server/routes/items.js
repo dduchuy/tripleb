3815d6cac7318781d186a36ab679b600d34e7cd6
duchuyddh <ddh9999@uw.edu>
2017-11-05 17:18:20 -0800

    updated readMe

--- server/routes/items.js
+++
@@ -1,8 +0,0 @@
-app.get('/items', function (req, res) {
-    Item.find({}, function (err, items) {
-        if (err)
-            throw err;
-        res.send(items);
-    });
-});
-//# sourceMappingURL=items.js.map
